bd880971c0ba32d147f32c9c8fd3ea6b05eb1296
Nicolás Vera <nicolasveca@hotmail.com>
2023-02-16 20:22:28 +0100

    Añadidas entradas con enlace a su página

---
+++ Controllers/EntradaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapp.Models;
+
+namespace webapp.Controllers
+{
+    public class EntradaController : Controller
+    {
+        private readonly WebappDbContext _db;
+        public EntradaController(WebappDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Entrada> objEntrada = _db.Entradas;
+            return View(objEntrada);
+        }
+
+        public IActionResult IrA(int numpag = 1)
+        {
+            IList<Pagina> OnePage = new List<Pagina>
+            {
+                new Pagina(){Id = numpag, URL = "https://simonet.blob.core.windows.net/paginas/" + numpag + ".jpg"}
+            };
+            return View(OnePage);
+        }
+    }
+}
